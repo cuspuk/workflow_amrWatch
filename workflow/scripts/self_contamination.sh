1e0c778438ddc1fe03a1db85814341f0c2d1c135
sitarcik <jozef.sitarcik@geneton.sk>
2024-01-15 19:27:03 +0100

    fixed comparison logic for selfcontamination

--- workflow/scripts/self_contamination.sh
+++ workflow/scripts/self_contamination.sh
@@ -10,7 +10,7 @@ mkdir -p "$out_dir" 2>> "${snakemake_log[0]}";
 
 if [ "$rows" -gt "${snakemake_params[max_rows]}" ]
 then
-    echo "PASS: Number of ambiguous rows fulfills the criteria ($rows<=${snakemake_params[max_rows]})" > "${snakemake_output[0]}" 2>> "${snakemake_log[0]}"
-else
     echo "FAIL: Number of ambiguous rows is higher than the defined threshold ($rows>${snakemake_params[max_rows]})" > "${snakemake_output[0]}" 2>> "${snakemake_log[0]}"
+else
+    echo "PASS: Number of ambiguous rows fulfills the criteria ($rows<=${snakemake_params[max_rows]})" > "${snakemake_output[0]}" 2>> "${snakemake_log[0]}"
 fi
